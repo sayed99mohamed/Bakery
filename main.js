ed21ff2e126bf301ae98a9eecd914475378516a3
sayed99mohamed <130188791+sayed99mohamed@users.noreply.github.com>
2026-07-21 20:28:09 +0300

    Make It Responsive

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-document.querySelector("p").style.color= "blue"
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+
+});
+
+const links = document.querySelectorAll("#nav-links li");
+
+links.forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navLinks.classList.remove("active");
+        menuToggle.classList.remove("active");
+
+    });
+
+});
